a5d9130347eec2b5789ce33ce12efb9de8dfc740
Cha Seoyeon <seoyeon0833@gmail.com>
2024-11-28 20:31:32 +0900

    적 오브젝트 이동/공격 애니메이션 구현

--- Assets/Scripts/EnemyMoving.cs
+++ Assets/Scripts/EnemyMoving.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EnemyMoving : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] float moveSpeed = 4f;
+    [SerializeField] GameObject target;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float distance = 1f;
+    [SerializeField] float attackDelayTime = 2f;
 
     private Animator anim;
     private Vector3 moveDirection;
+    private bool isAttacking = false;
 
     private void Start()
     {
@@ -21,9 +24,10 @@ public class EnemyMoving : MonoBehaviour
         EnemyMove();
     }
 
+    // 적 오브젝트가 바라보는 방향 설정
     private void SetDirection()
     {
-        moveDirection = (player.transform.position - transform.position).normalized;
+        moveDirection = (target.transform.position - transform.position).normalized;
 
         anim.SetFloat("Horizontal", moveDirection.x);
         anim.SetFloat("Vertical", moveDirection.y);
@@ -31,7 +35,28 @@ public class EnemyMoving : MonoBehaviour
 
     private void EnemyMove()
     {
-        //Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);
+        // 타겟과 적 오브젝트간 거리가 Epsilon 값보다 작을 경우 적 오브젝트는 제자리에 존재하며 타겟을 공격.
+        if ((target.transform.position - transform.position).magnitude < distance)
+        {
+            StartCoroutine(EnemyAttack(target));
+            anim.SetBool("IsWalking", false);
+            return;
+        }
+
+        anim.SetBool("IsWalking", true);
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator EnemyAttack(GameObject attackTarget)
+    {
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            anim.SetTrigger("IsAttacking");
+            // 타겟 오브젝트의 체력을 깎는 메소드 삽입 예정
+            yield return new WaitForSeconds(attackDelayTime);
+            anim.ResetTrigger("IsAttacking");
+            isAttacking = false;
+        }
     }
 }
