3d937b1f202d12f371f23e55a4380630d851db4e
Cha Seoyeon <seoyeon0833@gmail.com>
2025-02-01 20:47:23 +0900

    미니맵 생성

--- Assets/Scripts/CameraMoving.cs
+++ Assets/Scripts/CameraMoving.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class CameraMoving : MonoBehaviour
 {
-    private Camera mainCamera;
+    [SerializeField] private Camera miniMapCamera;
     private GameObject player;
 
     private void Start()
     {
-        mainCamera = Camera.main;
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void LateUpdate()
     {
-        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, player.transform.position, .8f * Time.deltaTime);
+        miniMapCamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
     }
 }
